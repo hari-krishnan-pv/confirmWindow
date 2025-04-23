8b82ce8e173c47cd2d4e3c4de6f1daddfd19b225
Harikrishnan PV <rafi@qdes.onmicrosoft.com>
2025-04-23 15:24:19 +0530

    test

--- src/lib/src/cool-dialogs.module.ts
+++ src/lib/src/cool-dialogs.module.ts
@@ -1,26 +1,17 @@
-import { NGX_QUICK_DIALOG_CONFIG } from './cool-dialogs.config';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxCoolDialog } from './cool-dialog';
 import { NgxCoolDialogsService } from './cool-dialogs.service';
-import { NgxCoolDialogsGlobalConfig } from './cool-dialogs.config';
+import { NgxCoolDialogsGlobalConfig, NGX_QUICK_DIALOG_CONFIG } from './cool-dialogs.config';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [
-    NgxCoolDialog
-  ],
-  exports: [
-    NgxCoolDialog
-  ],
-  entryComponents: [
-    NgxCoolDialog
-  ]
+  imports: [CommonModule],
+  declarations: [NgxCoolDialog],
+  exports: [NgxCoolDialog],
+  entryComponents: [NgxCoolDialog] // optional in Angular 9+, remove if Ivy is enabled
 })
 export class NgxCoolDialogsModule {
-  static forRoot(globalConfig?: NgxCoolDialogsGlobalConfig): ModuleWithProviders {
+  static forRoot(globalConfig?: NgxCoolDialogsGlobalConfig): ModuleWithProviders<NgxCoolDialogsModule> {
     return {
       ngModule: NgxCoolDialogsModule,
       providers: [
